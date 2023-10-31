f2c0a09d30039d777721e0a579843ef6c25e6b87
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-10-31 15:30:14 +0630

    finished banner component

--- airbnb-clone/components/Banner.jsx
+++ airbnb-clone/components/Banner.jsx
@@ -1,7 +1,22 @@
+import Image from "next/image"
+
 const Banner = () => {
     return (
-        <div>
-            <h1 className="mt-5 mb-5 text-xl font-semibold text-yellow-300">I'm a banner.</h1>
+        <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
+            <Image
+                src="https://links.papareact.com/0fm"
+                layout="fill"
+                objectFit="cover"
+            />
+            <div className="absolute top-1/2 w-full text-center">
+                <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
+
+                <button className="text-purple-500 bg-white px-10 py-4 
+                    rounded-full shadow-md font-bold my-3 hover:shadow-xl
+                    active:scale-95 transition duration-150">
+                    I'm flexible
+                </button>
+            </div>
         </div>
     )
 }
